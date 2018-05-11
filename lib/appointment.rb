bee4b5eea784c859a87a57e9600d5d9999d19b2c
Spencer Goodman <spencerwgoodman@icloud.com>
2018-05-11 03:22:22 +0000

    automatically backed up by learn

---
+++ lib/appointment.rb
@@ -0,0 +1,13 @@
+
+class Appointment
+  attr_accessor :doctor, :patient, :time
+
+  def initialize(time, doctor)
+    @time = time
+    @doctor = doctor
+  end
+
+
+
+
+end
--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,12 +1,21 @@
 
 class Doctor
-  attr_accessor :name, :patients, :appointment
+  attr_accessor :name, :patients, :appointments
 
-  def initialize(appointment, name)
-    @appointment = appointment
+  def initialize(name)
     @name = name
+    @patients = []
+    @appointments = []
   end
 
+  def add_appointment(appointment)
+    @appointments << appointment
+    appointment.doctor = self
+  end
 
-
+  def appointments
+    self.appointments.collect do |appointments|
+      appointments.patients
+    end
+  end
 end
