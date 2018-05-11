394442d37acf0e0ea74aeeabbe0140a2c6ec7af8
Spencer Goodman <spencerwgoodman@icloud.com>
2018-05-11 03:35:33 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,9 +1,16 @@
 
 class Patient
-  attr_accessor :name
+  attr_accessor :name, :appointments
 
   def initialize(name)
     @name = name
     @appointments = []
   end
+
+  def doctors
+    @appointments.collect do |appointment|
+      appointment.doctor
+    end
+  end
+
 end
